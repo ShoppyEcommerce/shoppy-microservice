4eb7c90905816932998e5f8c47893dc0e9e4a64d
okechukwu26 <priceblings@gmail.com>
2024-01-31 11:15:20 +0100

    init

--- vendor/src/app.ts
+++ vendor/src/app.ts
@@ -1,12 +1,12 @@
 import { Application, json } from "express";
 import cors from "cors";
-import helmet from "helmet";
+
 import { Utils } from "./utils";
 import { Delivery, Product, Vendor, appEvents } from "./api";
 
 export default async (app: Application) => {
   app.use(cors());
-  app.use(helmet());
+
   app.use(json());
   appEvents(app);
   const channel = await Utils.CreateChannel();
