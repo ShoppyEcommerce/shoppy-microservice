05bb4f26edf540ff90cab40257c364dec72da32f
okechukwu26 <priceblings@gmail.com>
2024-01-31 10:44:06 +0100

    init

---
+++ user/src/database/model/index.ts
@@ -0,0 +1,5 @@
+export { User, UserModel } from "./user";
+export { Category, CategoryModel } from "./category";
+export { Module, ModuleModel } from "./module";
+export { Order, OrderModel } from "./order";
+// export { Favorite, FavoriteModel } from "./favorite";
